2728271f68559dd1d3ec2746dc384a74d87b19cd
jawadkhan54 <122193707+jawadkhan54@users.noreply.github.com>
2023-01-21 14:57:55 -0600

    summary 3

--- students/models.py
+++ students/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
 
-def __str__(self) -> str:
-    return self.first_name + " " + self.last_name
+    def __str__(self) -> str:
+        return self.first_name + " " + self.last_name
